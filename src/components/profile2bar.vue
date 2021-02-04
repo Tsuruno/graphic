9b238f5c0488b0a44488b61cb0efa69e01df5f82
Tsuruno <bluehearts@outlook.com>
2021-02-04 23:09:44 +0900

    new

--- src/components/profile2bar.vue
+++ src/components/profile2bar.vue
@@ -1,17 +1,44 @@
 <template>
 <div>
-  <b-img class="w-100 h-25" v-bind="header" src="https://picsum.photos/1024/400/?image=41" alt="header"></b-img>
+  <div>
+    <b-img-lazy v-bind="barner" :src="getImageUrl(80)" alt="barner"></b-img-lazy>
 
-  <!-- <img class="rounded-circle" alt="100x100" src="..." data-holder-rendered="true"> -->
+    <b-img v-bind="icon" :img-src="loadImg(logo.png)" alt="icon"></b-img>
+  </div>
+
 </div>
 </template>
 
-// <script>
-//   export default {
-//     data() {
-//       return {
-//         header:{width: auto, height: 200}
-//       }
-//     }
-//   }
-// </script>
+<script>
+export default {
+  data() {
+    return {
+      barner: {
+        center: true,
+        fluidGrow: true,
+        blank: true,
+        blankColor: '#bbb',
+        width: 600,
+        height: 100
+      },
+      icon: {
+        center: true,
+        width: 100,
+        height: 100
+      }
+    }
+  },
+  methods: {
+    getImageUrl(imageId) {
+      const {
+        width,
+        height
+      } = this.barner
+      return `https://picsum.photos/${width}/${height}/?image=${imageId}`
+    },
+    loadImg(filename) {
+      return require(`../assets/${filename}`)
+    }
+  }
+}
+</script>

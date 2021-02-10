c3e3b2453e2a54a2d95e539f10552f8446b1ba1e
Tsuruno <bluehearts@outlook.com>
2021-02-10 23:37:11 +0900

    new

--- src/components/selectbutton.vue
+++ src/components/selectbutton.vue
@@ -1,6 +1,44 @@
+<template>
 <div>
-  <b-button>Button</b-button>
-  <b-button variant="danger">Button</b-button>
-  <b-button variant="success">Button</b-button>
-  <b-button variant="outline-primary">Button</b-button>
+  <b-button-group>
+      <b-button
+        pill
+        v-bind="button"
+        v-for="(btn, idx) in buttons"
+        :key="idx"
+        :pressed.sync="btn.state"
+        variant="outline-secondary"
+      >
+        {{ btn.caption }}
+      </b-button>
+    </b-button-group>
+    <p>Pressed States: <strong>{{ btnStates }}</strong></p>
+    <router-link to="/test3">test3</router-link>
 </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      button: {
+        width: 100
+      },
+      buttons: [{
+          caption: 'artwork',
+          state: true,
+          to: "/test1"
+        },
+        {
+          caption: 'Production',
+          state: false
+        }]
+    }
+  },
+  computed: {
+    btnStates() {
+      return this.buttons.map(btn => btn.state)
+    }
+  }
+}
+</script>

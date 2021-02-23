cc8817efaedab96cd4090ba469693b5dc8dd8556
Tsuruno <bluehearts@outlook.com>
2021-02-23 18:14:41 +0900

    new

--- src/components/artwork.vue
+++ src/components/artwork.vue
@@ -1,40 +1,48 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4">
+  <b-container-fluid class="bv-example-row">
+    <b-row cols="12">
+      <b-col>
         <div class="img_wrap">
-          <img
-            src="https://cotodama.co/wp-content/uploads/2018/09/css_mouseover_img03.png"
-          />
+          <img class="img_size" src="../assets/artwork/namahamu.jpg" />
           <div class="mask">
             <div class="caption">flesh souse</div>
           </div>
         </div>
-      </div>
-      <div class="col-md-4">2</div>
-      <div class="col-md-4">3</div>
-      <div class="col-md-4">4</div>
-      <div class="col-md-4">5</div>
-      <div class="col-md-4">6</div>
-      <div class="col-md-4">7</div>
-      <div class="col-md-4">8</div>
-      <div class="col-md-4">9</div>
-      <div class="col-md-4">10</div>
-      <div class="col-md-4">11</div>
-      <div class="col-md-4">12</div>
-      <div class="col-md-4">13</div>
-    </div>
-  </div>
+      </b-col>
+      <b-col>
+        <div class="img_wrap">
+          <img class="img_size" src="../assets/artwork/namahamu.jpg" />
+          <div class="mask">
+            <div class="caption">flesh souse</div>
+          </div>
+        </div>
+      </b-col>
+      <b-col>
+        <div class="img_wrap">
+          <img class="img_size" src="../assets/artwork/namahamu.jpg" />
+          <div class="mask">
+            <div class="caption">flesh souse</div>
+          </div>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container-fluid>
 </template>
 
 <script></script>
 
 <style>
+b-col {
+  margin: 10px 10px 10px 10px;
+}
+.img_size {
+  width: 100%;
+  height: auto;
+}
 .img_wrap {
-  width: 280px;
-  height: 188px;
+  width: 100%;
+  height: auto;
   overflow: hidden;
-  margin: 10px 8px 10px 16px;
   position: relative; /* 相対位置指定 */
 }
 .img_wrap .caption {
@@ -51,15 +59,6 @@
   left: 0;
   opacity: 0; /* マスクを表示しない */
   background-color: rgba(0, 0, 0, 0.4); /* マスクは半透明 */
-  -webkit-transition: all 0.2s ease;
-  transition: all 0.2s ease;
-}
-.mask {
-  width: 100%;
-  height: 100%;
-  position: absolute; /* 絶対位置指定 */
-  top: 50px;
-  left: 0;
   -webkit-transition: all 0.2s ease;
   transition: all 0.2s ease;
 }
